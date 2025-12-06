62ca2b138d2be178928c10b36668e16bd5393786
rzabka1 <jturovska@gmail.com>
2025-12-06 14:14:56 +0100

    Small changes
    Added very helpful comments to player script, added "good job" label for moral support

--- tilemap-layers/Scripts/rigid_player.gd
+++ tilemap-layers/Scripts/rigid_player.gd
@@ -1,12 +1,16 @@
 extends RigidBody2D
 
+@onready var raycast: RayCast2D = $RayCast2D
+
 var angle
+var do_not_touch_this_fuckass_value = 89.54  #idk why this exists
+var jump_velocity = -40000
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		var speed = 500
-		if angle != null and angle < 75 and angle > -75:
+		if angle != null and abs(angle) < 75:
 			if direction < 0:
 				speed += angle * 30
 			elif direction > 0:
@@ -14,31 +18,32 @@ func _physics_process(_delta: float) -> void:
 		else:
 			speed = speed / 2
 		apply_central_force(Vector2(direction*speed,0))
-	if Input.is_action_just_pressed("Jump") and angle != null and angle < 75 and angle > -75:
-			apply_central_force(Vector2(0,-40000))
-	$RayCast2D.rotation = -rotation + 89.54
+	if Input.is_action_just_pressed("Jump") and angle != null and abs(angle) < 75:
+			apply_central_force(Vector2(0, jump_velocity))
+	raycast.rotation = -rotation + do_not_touch_this_fuckass_value
 
 
 func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
 	var _contacted = get_colliding_bodies()
 	if _contacted.size() > 0:
 		if _state.get_contact_count() > 1:
-			var closest_to_zero:float = 361
+			var closest_to_zero:float = 361 #idk what this is but changing this causes problems going up the slopes
 			for i in _state.get_contact_count():
 				var pos = _state.get_contact_collider_position(i)
-				var one_of_angles = rad_to_deg($RayCast2D.get_angle_to(pos))
+				var one_of_angles = rad_to_deg(raycast.get_angle_to(pos))
 				if 0-abs(closest_to_zero) < 0-abs(one_of_angles):
 					closest_to_zero = one_of_angles
 			angle = closest_to_zero
 		else:
 			var pos = _state.get_contact_collider_position(0)
-			angle = rad_to_deg($RayCast2D.get_angle_to(pos))
+			angle = rad_to_deg(raycast.get_angle_to(pos))
 	else:
 		angle = null
 
 
 func _on_spike_detecting_area_body_entered(_body: Node2D) -> void:
 	die()
+
 
 func die():
 	print("died")
