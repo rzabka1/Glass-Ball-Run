e27b201e34a7ad51f9bee975ad979bc881dd8e59
rzabka1 <jturovska@gmail.com>
2025-12-30 23:59:12 +0100

    Quick commit :>

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,35 +2,57 @@ class_name Level
 extends Node2D
 
 var player:RigidBody2D
+var checkpoint_start:Checkpoint
 @onready var player_respawn_timer:Timer = $PlayerRespawnTimer
 @onready var cam:Camera2D = $Camera2D
 @onready var player_scene:PackedScene = preload("res://Scenes/rigid_player.tscn")
 @onready var collectibles_parent:Node2D = $Collectibles
 @onready var checkpoint_parent: Node2D = $Checkpoints
+@onready var bg_color: ColorRect = $Bckgr/ColorRect
 
 func _ready() -> void:
 	Global.all_collectibles = collectibles_parent.get_children()
 	Global.set_max_coins()
+	checkpoint_start = checkpoint_parent.get_node("CheckpointStart")
 	assign_checkpoint_id()
 	assign_collectible_id()
+	set_level_color()
 
 func assign_checkpoint_id():
-	var all_checkpoints:Array = checkpoint_parent.get_children()
-	var checkpoint_start:Checkpoint = checkpoint_parent.get_node("CheckpointStart")
-	all_checkpoints.erase(checkpoint_start)
+	var checkpoint_arr:Array = get_all_checkpoints()
 	checkpoint_start.id = -1
-	for checkp in all_checkpoints:
-		checkp.id = all_checkpoints.find(checkp)
+	for checkp in checkpoint_arr:
+		checkp.id = checkpoint_arr.find(checkp)
 		print("Chp ID: ", checkp.id)
 
 func assign_collectible_id():
-	var flags:Array = []
+	var flag_arr:Array = get_all_flags()
+	for flag in flag_arr:
+		flag.id = flag_arr.find(flag)
+		print("Flag ID: ", flag.id)
+
+func get_all_checkpoints() -> Array:
+	var all_checkpoints:Array = checkpoint_parent.get_children()
+	all_checkpoints.erase(checkpoint_start)
+	return all_checkpoints
+
+func get_all_flags() -> Array:
+	var all_flags:Array = []
 	for collectible in Global.all_collectibles:
 		if collectible.collectible_type == collectible.type.FLAG:
-			flags.append(collectible)
-	for flag in flags:
-		flag.id = flags.find(flag)
-		print("Flag ID: ", flag.id)
+			all_flags.append(collectible)
+	return all_flags
+
+func set_level_color():
+	if get_all_checkpoints().size() == get_all_flags().size():
+		for i in get_all_checkpoints().size():
+			var level_color_arr = ColorLib.pick_level_color()
+			get_all_checkpoints()[i].modulate = level_color_arr[1]
+			get_all_flags()[i].modulate = level_color_arr[1]
+
+
+
+
 
 func _process(_delta: float) -> void:
 	glue_camera_to_player()
