0eaeaceded53fb8dedb2c5d0c248edf449d9125a
rzabka1 <jturovska@gmail.com>
2026-01-07 23:49:10 +0100

    ColorLib script refactor

--- Scripts/ColorLib.gd
+++ Scripts/ColorLib.gd
@@ -1,9 +1,7 @@
 extends Node
 
-var current_color:Color
-var bg_lightened_value:float = 0.3 #how much lighter background will be than the flag
-# These below are actually *flag* colors
-var level_colors:Array = [
+# These are actually *flag* colors
+var level_colors:Array[Color] = [
 	Color(0.743, 0.463, 0.778, 1.0),
 	Color(0.99, 0.538, 0.663, 1.0),
 	Color(1.0, 0.33, 0.531, 1.0),
@@ -20,17 +18,38 @@ var level_colors:Array = [
 	Color(0.338, 0.714, 0.72, 1.0),
 	Color(0.544, 0.638, 0.8, 1.0)
 	]
+var current_color_index:int = -1
+var bg_lightened_value:float = 0.3 # How much lighter background will be than the flag
 
 func pick_level_color() -> Color:
-	var flag_color:Color
-	var current_id:int = level_colors.find(current_color)
-	var potential_colors:Array = []
-
-	for color in level_colors:
-		# Excluding similar colors
-		if level_colors.find(color) != current_id and level_colors.find(color) != current_id-1 and level_colors.find(color) != current_id+1:
-			potential_colors.append(color)
-	flag_color = potential_colors.pick_random()
-	current_color = flag_color
+	# Safety check: If array is empty, return a default fallback
+	if level_colors.is_empty():
+		push_warning("Missing resource: level_colors array is empty")
+		return Color.BLACK
 	
-	return flag_color
+	var potential_indexes: Array[int] = []
+	
+	for i in level_colors.size():
+		# If current_color_index is -1 (first run), accept everything
+		if current_color_index != -1:
+			var distance = abs(i - current_color_index)
+			
+			# Exclude current (0) and immediate neighbours (1)
+			if distance <= 1:
+				continue # continue = ignore all further instructions and go to the next iteration :>
+			
+			# WRAPPING LOGIC (Circular Array)
+			# e.g., if array size is 15, index 0 and index 14 are neighbours.
+			if distance == level_colors.size() - 1:
+				continue
+		
+		potential_indexes.append(i)
+	
+	# If potential color array is empty, pick the current color again
+	if potential_indexes.is_empty():
+		potential_indexes.append(current_color_index)
+	
+	var new_index: int = potential_indexes.pick_random()
+	current_color_index = new_index
+	
+	return level_colors[new_index]
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -5,11 +5,6 @@ extends CanvasLayer
 
 
 func _on_bg_check_button_toggled(toggled_on: bool) -> void:
-	match toggled_on:
-		true:
-			platform_bg_texture.visible = true
-			platform_bg_color.visible = false
-		false:
-			platform_bg_texture.visible = false
-			platform_bg_color.visible = true
+	platform_bg_texture.visible = toggled_on
+	platform_bg_color.visible = not toggled_on
 	
